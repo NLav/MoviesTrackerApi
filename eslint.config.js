78fea24d3893caea5e6e5026b6ffe60b63c0576a
Nicolas Lavoura <nicolas.lavoura@teamsoft.com.br>
2025-08-09 21:47:57 -0300

    refactor: improve eslint configs and lint project

---
+++ eslint.config.js
@@ -0,0 +1,45 @@
+import js from "@eslint/js";
+import { globalIgnores } from "eslint/config";
+import eslintPluginImport from "eslint-plugin-import";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import eslintPluginUnicorn from "eslint-plugin-unicorn";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+// eslint-disable-next-line import/no-default-export
+export default tseslint.config([
+  globalIgnores(["dist"]),
+  {
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      eslintPluginPrettierRecommended,
+      eslintPluginImport.flatConfigs.typescript,
+      eslintPluginUnicorn.configs.all,
+    ],
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    rules: {
+      "import/no-default-export": "error",
+      "prettier/prettier": "error",
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      "unicorn/filename-case": [
+        "error",
+        {
+          cases: {
+            pascalCase: true,
+            camelCase: true,
+          },
+        },
+      ],
+      "unicorn/no-keyword-prefix": "off",
+    },
+  },
+]);
