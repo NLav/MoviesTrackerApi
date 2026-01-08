a665044690361fb2172cd6277f60ff494ebfd6f9
Nicolas Lavoura <nicolas.lavoura@teamsoft.com.br>
2026-01-08 04:26:54 -0300

    fix: fix create genres migration

--- src/infrastructure/persistence/typeorm/migrations/1755298804389-create-genres-table.ts
+++ src/infrastructure/persistence/typeorm/migrations/1755298804389-create-genres-table.ts
@@ -5,7 +5,7 @@ export class CreateGenresTable1755298804389 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE \`genres\` (\`id\` varchar(36) NOT NULL, \`name\` varchar(255) NOT NULL, \`created_at\` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updated_at\` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`deleted_at\` timestamp(6) NULL, UNIQUE INDEX \`IDX_f105f8230a83b86a346427de94\` (\`name\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`
+      `CREATE TABLE IF NOT EXISTS \`genres\` (\`id\` varchar(36) NOT NULL, \`name\` varchar(255) NOT NULL, \`created_at\` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updated_at\` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`deleted_at\` timestamp(6) NULL, UNIQUE INDEX \`IDX_f105f8230a83b86a346427de94\` (\`name\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`
     );
   }
 
